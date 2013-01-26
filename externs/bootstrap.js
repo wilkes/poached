ecd4cdb541c0c9f93f7b94e75283e4264b29c852
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-01-26 16:10:47 -0600

    added boostrap and jquery externs

---
+++ externs/bootstrap.js
@@ -0,0 +1,14 @@
+function jQuery(arg1, arg2) {};
+
+jQuery.prototype.fn.modal = function(option) {};
+jQuery.prototype.fn.dropdown = function(option) {};
+jQuery.prototype.fn.scrollspy = function(option) {};
+jQuery.prototype.fn.tab = function(option) {};
+jQuery.prototype.fn.tooltip = function(option) {};
+jQuery.prototype.fn.popover = function(option) {};
+jQuery.prototype.fn.alert = function(option) {};
+jQuery.prototype.fn.button = function(option) {};
+jQuery.prototype.fn.collapse = function(option) {};
+jQuery.prototype.fn.carousel = function(option) {};
+jQuery.prototype.fn.typeahead = function(option) {};
+jQuery.prototype.fn.affix = function(option) {};
